0e90ede47117ce6d89f58d8ff758c41b3d2e8736
Jhonatan Ferreira <jhonatan.ferreira@live.com>
2026-06-16 17:06:31 -0300

    chore: inicio da listagem dos produtos

--- app/mocks/states.ts
+++ app/mocks/states.ts
@@ -1,3 +1,6 @@
-export const STATES: Array<any> =[
-    {acronym: "SC", name: "Floripa"}
+export const STATES: Array<any> = [
+    { acronym: "SC", name: "Floripa" },
+    { acronym: "GO", name: "Goiás" },
+    { acronym: "SP", name: "São Paulo" },
+    { acronym: "RJ", name: "Rio de Janeiro" }
 ]
