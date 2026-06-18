1369befe10906cec820ff377fd8c76c31195ecf2
Jhonatan Ferreira <jhonatan.ferreira@live.com>
2026-06-18 17:13:59 -0300

    chore: Produtos

--- app/mocks/products.ts
+++ app/mocks/products.ts
@@ -1,66 +1,74 @@
 export const PRODUCTS: Array<IProduct> = [
     {
         id: 1,
+        photo: "https://e7.pngegg.com/pngimages/887/86/png-clipart-silver-dell-laptop-dell-xps-13-9350-laptop-computer-laptop-electronics-netbook.png",
         categoryID: 1,
-        name: "Produto 01",
+        name: "Notebook 12'",
         description: "Descrição 01",
         sellerID: 1,
-        price: 25.55
+        price: Math.floor(Math.random() * (1000 - 100 + 1)) + 100
     },
     {
         id: 2,
+        photo: "https://novomundo.vtexassets.com/arquivos/ids/28237422-800-800?v=638998539089730000&width=800&height=800&aspect=true",
         categoryID: 2,
         name: "Produto 02",
         description: "Descrição 02",
         sellerID: 2,
-        price: 25.55
+        price: 20.99
     },
     {
         id: 3,
+        photo: "",
         categoryID: 3,
         name: "Produto 03",
         description: "Descrição 03",
         sellerID: 3,
-        price: 25.55
+        price: 22.45
     },
     {
         id: 4,
+        photo: "",
         categoryID: 4,
         name: "Produto 04",
         description: "Descrição 04",
         sellerID: 4,
-        price: 25.55
+        price: 15.00
     },
     {
         id: 5,
+        photo: "",
         categoryID: 5,
         name: "Produto 05",
         description: "Descrição 05",
         sellerID: 5,
-        price: 25.55
+        price: 12.75
     },
     {
         id: 6,
+        photo: "",
         categoryID: 6,
         name: "Produto 06",
         description: "Descrição 06",
         sellerID: 6,
-        price: 25.55
+        price: 35.99
     },
     {
         id: 7,
+        photo: "",
         categoryID: 7,
         name: "Produto 07",
         description: "Descrição 07",
         sellerID: 7,
-        price: 25.55
+        price: 17.14
     },
     {
         id: 8,
+        photo: "",
         categoryID: 8,
         name: "Produto 08",
         description: "Descrição 08",
         sellerID: 8,
-        price: 25.55
+        price: 45.98
     }
 ]
